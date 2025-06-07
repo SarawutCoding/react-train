5caf5139f7657de6e68cc8697316792279f3be0c
SarawutCoding <sarawutphuangsumalee@gmail.com>
2025-06-07 19:51:29 +0700

    save

--- src/components/Addfrom.jsx
+++ src/components/Addfrom.jsx
@@ -4,11 +4,18 @@ import { useState } from "react"
 export default function Addfrom() {
     const [name, setName]=useState("");
     const [gender, setGender]=useState("");
-    
+    function saveData(e) {
+        e.preventDefault();
+        const user ={
+            name:name,
+            gender:gender
+        }
+        console.log(user);
+    }
     return(
         <>
             <section className="container">
-                <form action="">
+                <form onSubmit={saveData}>
                     <label htmlFor="">ชื่อประชากร</label>
                     <input type="text" name="" id="" value={name} onChange={(e)=>setName(e.target.value)}/>
                     <select name="" id="" value={gender} onChange={(e)=>setGender(e.target.value)}>
